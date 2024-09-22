79f282346fda94dbafe90a23b57d6c0fc6f673c6
aliamaulana <aliaresti11@gmail.com>
2024-09-22 14:41:51 +0700

    acara5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
+import { Geolocation } from '@capacitor/geolocation';
 import Graphic from '@arcgis/core/Graphic';
 import Point from '@arcgis/core/geometry/Point';
-import PictureMarkerSymbol from '@arcgis/core/symbols/PictureMarkerSymbol'; // Import PictureMarkerSymbol
-import { Geolocation } from '@capacitor/geolocation';
+import PictureMarkerSymbol from '@arcgis/core/symbols/PictureMarkerSymbol'; // Use picture marker
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
 
 @Component({
   selector: 'app-home',
@@ -12,49 +13,56 @@ import { Geolocation } from '@capacitor/geolocation';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  mapView: MapView | any;
+  userLocationGraphic: Graphic | any;
+  map: Map | any;
+  latitude: number = 36.40737347996374;
+  longitude: number = -97.90088176361843;
 
-  constructor() { }
+  constructor() {}
 
-  private latitude: number | any;
-  private longitude: number | any;
-
-  public async ngOnInit() {
-    const position = await Geolocation.getCurrentPosition();
-    this.latitude = position.coords.latitude;
-    this.longitude = position.coords.longitude;
-
-    const map = new Map({
-      basemap: "topo-vector"
+  async ngOnInit() {
+    this.map = new Map({
+      basemap: 'topo-vector' // Default basemap
     });
 
-    const view = new MapView({
-      container: "container",
-      map: map,
-      zoom: 20,
+    this.mapView = new MapView({
+      container: 'container',
+      map: this.map,
+      zoom: 8,
       center: [this.longitude, this.latitude]
     });
 
-    // Menggunakan PictureMarkerSymbol untuk marker custom
-    const customMarkerSymbol = new PictureMarkerSymbol({
-      url: "https://cdn-icons-png.flaticon.com/512/684/684908.png",  // URL gambar
-      width: "32px",  // Lebar gambar marker
-      height: "32px"  // Tinggi gambar marker
-    });
+    let weatherServiceFL = new ImageryLayer({ url: WeatherServiceUrl });
+    this.map.add(weatherServiceFL);
 
-    // Membuat objek Point untuk lokasi sekarang
-    const point = new Point({
-      longitude: this.longitude,
-      latitude: this.latitude
-    });
-
-    // Membuat Graphic untuk marker dengan simbol gambar
-    const pointGraphic = new Graphic({
-      geometry: point,
-      symbol: customMarkerSymbol  // Terapkan simbol gambar
-    });
-
-    // Tambahkan marker ke view
-    view.graphics.add(pointGraphic);
+    this.addMarkerAtLocation(this.latitude, this.longitude);
   }
 
+  // Function to add a marker at the specified coordinates
+  addMarkerAtLocation(lat: number, long: number) {
+    const point = new Point({ latitude: lat, longitude: long });
+
+    // Use PictureMarkerSymbol to display a custom image as the marker
+    const symbol = new PictureMarkerSymbol({
+      url: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', // URL of the image
+      width: '32px',  // width of the image
+      height: '32px', // height of the image
+    });
+
+    this.userLocationGraphic = new Graphic({
+      geometry: point,
+      symbol: symbol
+    });
+
+    this.mapView.graphics.add(this.userLocationGraphic); // Add the marker to the map
+  }
+
+  // Function to handle basemap change
+  onBasemapChange(event: any) {
+    const selectedBasemap = event.detail.value;
+    this.map.basemap = selectedBasemap; // Change the basemap
+  }
 }
+
+const WeatherServiceUrl = 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer';
